3c092a0dcabc628911cad22f21f1910d890edbec
Myron <josephonyekachi77@gmail.com>
2023-04-06 05:20:47 -0800

    updated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,5 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react'
+import { Container } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
 import './Navbar.css'
 
@@ -12,49 +13,51 @@ function Navbar() {
 
     return (
         <>
-            <nav>
-                <Link to='/' className='nav-logo' onClick={closeMobileMenu}>
-                    KamTech
-                </Link>
+            <Container>
+                <nav>
+                    <Link to='/' className='nav-logo' onClick={closeMobileMenu}>
+                        KamTech
+                    </Link>
 
-                <div className='menu-icon' onClick={handleClick}>
-                    
-                    <img className='menu' src={click ? 'img/cancel-1.png' : 'img/menu-1.png'} alt="KamTech" />
+                    <div className='menu-icon' onClick={handleClick}>
 
-                </div>
+                        <img className='menu' src={click ? 'img/cancel-1.png' : 'img/menu-1.png'} alt="KamTech" />
 
-                <div className="menu-list">
-                    <ul className={click ? 'nav-menu active' : 'nav-menu'}>
-                        
-                        <li className='nav-item'>
-                            <Link to="" className='nav-link' onClick={closeMobileMenu}>
-                                Home
-                            </Link>
-                        </li>
-                        <li className='nav-item'>
-                            <Link to="" className='nav-link' onClick={closeMobileMenu}>
-                                About
-                            </Link>
-                        </li>
-                        <li className='nav-item'>
-                            <Link to="" className='nav-link' onClick={closeMobileMenu}>
-                                Service
-                            </Link>
-                        </li>
-                        <li className='nav-item'>
-                            <Link to="" className='nav-link' onClick={closeMobileMenu}>
-                                Project
-                            </Link>
-                        </li>
-                        <li className='navBtn'>
-                            <Link to="" className='nav-btn' onClick={closeMobileMenu}>
-                                Lets Connect
-                            </Link>
-                        </li>
-                    </ul>
-                </div>
+                    </div>
 
-            </nav>
+                    <div className="menu-list">
+                        <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+
+                            <li className='nav-item'>
+                                <Link to="" className='nav-link' onClick={closeMobileMenu}>
+                                    Home
+                                </Link>
+                            </li>
+                            <li className='nav-item'>
+                                <Link to="" className='nav-link' onClick={closeMobileMenu}>
+                                    About
+                                </Link>
+                            </li>
+                            <li className='nav-item'>
+                                <Link to="" className='nav-link' onClick={closeMobileMenu}>
+                                    Service
+                                </Link>
+                            </li>
+                            <li className='nav-item'>
+                                <Link to="" className='nav-link' onClick={closeMobileMenu}>
+                                    Project
+                                </Link>
+                            </li>
+                            <li className='navBtn'>
+                                <Link to="" className='nav-btn' onClick={closeMobileMenu}>
+                                    Lets Connect
+                                </Link>
+                            </li>
+                        </ul>
+                    </div>
+
+                </nav>
+            </Container>
         </>
     )
 }
